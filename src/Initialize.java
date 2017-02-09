a90d7e99febb3c9a904e5ebb8182b2090a08f6df
stott <stott@localhost>
2017-02-09 12:36:40 +0000

    Cleaned Initialize.java

--- src/Initialize.java
+++ src/Initialize.java
@@ -2,9 +2,11 @@
 
 public class Initialize {
     public static void main(String[] args) throws InterruptedException { 
-    
+    	
     	GUI browser = new GUI();
+    	
     	browser.close();
 
+    	
     } 
 }
