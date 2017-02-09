4910a1546d16224ec57ca8f9c9109a5bf79892cc
William Stott <wdstott@hotmail.co.uk>
2017-02-09 12:43:35 +0000

    Removed main from converter

--- src/Converter.java
+++ src/Converter.java
@@ -1,21 +1,13 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONObject;
 
 public class Converter {
-	
-	public static void main (String[] args){
-		HashMap<String, String> xz = new HashMap<String,String>();
-		xz.put("sdf", "sdf");
-		xz.put("test","fasf");
-		process(xz);
-	}
-	
-	public static void process(Map<String,String> x){
-		JSONObject json = new JSONObject(x);
-		System.out.println(json);
-		
+	public static String process(Map<String, List<String>> mapToConvert){
+		JSONObject json = new JSONObject(mapToConvert);
+		return json.toString();
 	}
 	
 }
