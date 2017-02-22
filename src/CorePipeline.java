3cc634105fa8792d89bbc6138065878fb0470506
William Stott <wdstott@hotmail.co.uk>
2017-02-22 17:34:34 +0000

    Finished converting the annotators to the new system. Core pipeline now only creates instances of selected annotators instead of all. On analyse asks the user for output location.

--- src/CorePipeline.java
+++ src/CorePipeline.java
@@ -12,31 +12,61 @@ import java.util.Map;
 import CorePipeline.Lemma;
 import CorePipeline.NER;
 import CorePipeline.POS;
+import CorePipeline.Parser;
 import CorePipeline.Sentiment;
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
-
-
 
 //https://blog.openshift.com/day-20-stanford-corenlp-performing-sentiment-analysis-of-twitter-using-java/ <-- read later for sentiment example
 public class CorePipeline 
 {
 	private ManageFile mf = new ManageFile();
-	private Sentiment sentiment = new Sentiment();
-	private POS pos = new POS();
-	private Lemma lem = new Lemma();
-	private NER ner = new NER();
+	
+	private Sentiment sentiment;
+	private POS pos;
+	private Lemma lem;
+	private NER ner;
+	private Parser parser;
+	
+	private List<String> selectedAnnotators;
+	
+	public CorePipeline(List<String> annotators)
+	{
+		this.selectedAnnotators = annotators;
+		
+		//Only creates an instance of the class if annotator is selected
+		for(String annotator : annotators)
+		{
+			switch(annotator)
+			{
+			case("NER"):
+				ner = new NER();
+				break;
+			case("Lemma"):
+				lem = new Lemma();
+				break;
+			case("POS"):
+				pos = new POS();
+				break;
+			case("Parser"):
+				parser = new Parser();
+				break;
+			case("Sentiment"):
+				sentiment = new Sentiment();
+				break;
+			
+			}
+		}
+	}
 
-	  public boolean processDataDir(Path dirLocation, List<String> selectedAnnotators, Path outputLocation, int outputMode)
-	  {
+	public boolean processDataDir(Path dirLocation, Path outputLocation, int outputMode)
+	{
 		try
 		{
 			ArrayList<Path> textFileLocations = new ArrayList<Path>();
 			textFileLocations = ManageFile.mapDirectory(dirLocation, new ArrayList<Path>(), ".txt");
-			
+
 			for(Path filePath : textFileLocations)
 			{
-				processData(filePath, selectedAnnotators, outputLocation, outputMode);
+				processData(filePath, outputLocation, outputMode);
 
 			}
 			return true;
@@ -45,141 +75,89 @@ public class CorePipeline
 		{
 			return false;
 		}
-	  }
-	 
-	  public boolean processData(Path fileLocation, List<String> selectedAnnotators, Path outputLocation, int outputMode)
-	  {
-       	try 
-       	{
-    	 	List<String> rawData = Files.readAllLines(fileLocation, StandardCharsets.UTF_8);
-    	 	
-    	 	//Creates a folder of the same name as file being processed		
-    	 	outputLocation = Paths.get(outputLocation.toString(), ManageFile.pathSlash, fileLocation.toString().substring(fileLocation.toString().lastIndexOf(ManageFile.pathSlash) + 1, fileLocation.toString().lastIndexOf('.')));
-    	 	if(!Files.exists(outputLocation))
-    	 	{
-    	 		Files.createDirectory(outputLocation);
-    	 	}
-    	 	
-    	 	mf.saveMapToFile(processData(rawData, selectedAnnotators), outputLocation);
+	}
 
-	    	return true;
-    	}
-    	catch(IOException e)
-    	{
-    		System.out.print(e);
-    		return false;
-    	}
-	  }
-	  
-	  public boolean processData(List<String> rawData, List<String> selectedAnnotators, Path outputLocaton)
-	  { 
-	  	  try
-	  	  {
-	  		mf.saveMapToFile(processData(rawData, selectedAnnotators), outputLocaton); 
-	  	  }
-	  	  catch(Exception e)
-	  	  {
-	  		  System.out.println(e);
-	  		  return false;
-	  	  }
-		  
-		  return true;
-	  }
-	  
-	  public Map<String, List<String>> processData(List<String> rawData, List<String> selectedAnnotators)
-	  { 
-	  		Map<String, List<String>> processedData = new HashMap<String, List<String>>();
-    		for(String annotator : selectedAnnotators)
-    		{
-    				if(!processedData.containsKey(annotator))
-    				{
-    					processedData.put(annotator, new ArrayList<String>());
-    				}
-    		}
- 		
-    		
-    		
-    		
-    		
-       		for(String line : rawData)
-       		{
-				if(line != null)
-				{		
-			        Document doc = new Document(line);
-			    	for (Sentence sent : doc.sentences()) 
-			    	{  
-
-	    				if(selectedAnnotators.contains("Parser"))
-	    				{
-	    					processedData.get("Parser").add(parseSentence(sent));
-	    				}
-	    				if(selectedAnnotators.contains("Lemma"))
-	    				{
-	    					processedData.get("Lemma").add(lem.process(sent.toString()));
-	    				}
-	    				if(selectedAnnotators.contains("POS"))
-	    				{
-	    					processedData.get("POS").add(pos.process(sent.toString()));
-	    				}
-	    				if(selectedAnnotators.contains("NER"))
-	    				{
-		    				processedData.get("NER").add(ner.process(sent.toString()));
-	    				}
-	    				if(selectedAnnotators.contains("Sentiment"))
-	    				{
-		    				processedData.get("Sentiment").add(String.valueOf(sentiment.process(sent.toString())));
-	    				}
-	    			}
-	    		}
-	        }
-       		return processedData;
-	   }
-	  /*
-		public int sentiment(String line)
+	public boolean processData(Path fileLocation, Path outputLocation, int outputMode)
+	{
+		try 
 		{
-	        Properties props = new Properties();
-	        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-	        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-	        int mainSentiment = 0;
-	        if (line != null && line.length() > 0) 
-	        {
-	            int longest = 0;
-	            Annotation annotation = pipeline.process(line);
-	            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) 
-	            {
-	                Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
-	                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-	                String partText = sentence.toString();
-	                if (partText.length() > longest) 
-	                {
-	                    mainSentiment = sentiment;
-	                    longest = partText.length();
-	                }
-	 
-	            }
-	        }
-	        return mainSentiment;
-	  }*/
+			List<String> rawData = Files.readAllLines(fileLocation, StandardCharsets.UTF_8);
 
-	  private String parseSentence(Sentence sentence)
-	  {
-		  return sentence.parse().toString();
-	  }
-	  
-	  private String lemmaSentence(Sentence sentence)
-	  {
-		  return sentence.lemmas().toString();
-	  }
-	  
-	  //http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
-	  private String posSentence(Sentence sentence)
-	  {
-		  return sentence.posTags().toString();
-	  }
+			//Creates a folder of the same name as file being processed		
+			outputLocation = Paths.get(outputLocation.toString(), ManageFile.pathSlash, fileLocation.toString().substring(fileLocation.toString().lastIndexOf(ManageFile.pathSlash) + 1, fileLocation.toString().lastIndexOf('.')));
+			
+			if(!Files.exists(outputLocation))
+			{
+				Files.createDirectory(outputLocation);
+			}
+
+			mf.saveMapToFile(processData(rawData), outputLocation);
+
+			return true;
+		}
+		catch(IOException e)
+		{
+			System.out.print(e);
+			return false;
+		}
+	}
+
+	public boolean processData(List<String> rawData, Path outputLocation)
+	{ 
+		try
+		{
+			
+			mf.saveMapToFile(processData(rawData), outputLocation); 
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			return false;
+		}
+
+		return true;
+	}
+
+	public Map<String, List<String>> processData(List<String> rawData)
+	{ 
+		Map<String, List<String>> processedData = new HashMap<String, List<String>>();
+		for(String annotator : selectedAnnotators)
+		{
+			if(!processedData.containsKey(annotator))
+			{
+				processedData.put(annotator, new ArrayList<String>());
+			}
+		}
+
+		for(String line : rawData)
+		{
+			if(line != null)
+			{		
+				
+				if(selectedAnnotators.contains("Parser"))
+				{
+					processedData.get("Parser").add(parser.process(line));
+				}
+				if(selectedAnnotators.contains("Lemma"))
+				{				
+					processedData.get("Lemma").add(lem.process(line));
+				}
+				if(selectedAnnotators.contains("POS"))
+				{
+					processedData.get("POS").add(pos.process(line));
+				}
+				if(selectedAnnotators.contains("NER"))
+				{
+					processedData.get("NER").add(ner.process(line));
+				}
+				if(selectedAnnotators.contains("Sentiment"))
+				{
+					processedData.get("Sentiment").add(String.valueOf(sentiment.process(line)));
+				}
+			}
+		}
+		return processedData;
+	}
 	
-	  private String nerSentence(Sentence sentence)
-	  {
-		  return sentence.nerTags().toString();
-	  }
 
 }
--- src/CorePipeline/Parser.java
+++ src/CorePipeline/Parser.java
@@ -1,5 +1,34 @@
 package CorePipeline;
 
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.simple.Document;
+import edu.stanford.nlp.simple.Sentence;
+
 public class Parser {
 
+	private Properties props = new Properties();
+	private StanfordCoreNLP pipeline;
+	
+	
+	public Parser()
+	{
+		props.setProperty("annotators", "tokenize, ssplit, pos");
+		pipeline = new StanfordCoreNLP(props);
+	}
+	
+	public String process(String text)
+	{
+	   Document doc = new Document(text);
+	   String parser = "";
+	   for(Sentence sent : doc.sentences())
+	   {
+		   parser += sent.parse().toString();
+	   }
+	   return parser.equals("") ? "Failed to process this line" : parser;
+	}
+	
 }
