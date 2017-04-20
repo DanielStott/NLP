8c79acd0baba31b2cd8854d2893461c88a62f69c
WilliamStott <wdstott@hotmail.co.uk>
2017-04-20 13:53:18 +0100

    Added comments

--- src/CorePipeline/DepParser.java
+++ src/CorePipeline/DepParser.java
@@ -10,30 +10,40 @@ import edu.stanford.nlp.process.DocumentPreprocessor;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 
+//This is the Dependancy parser annotator class
 public class DepParser {
 
 	private DependencyParser parser;
 	private MaxentTagger tagger;
 	private int lineNumb = 0;
 
+	//Adds properties when constructed
 	public DepParser()
 	{
 		parser = DependencyParser.loadFromModelFile(DependencyParser.DEFAULT_MODEL);
 		tagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
 	}
 
+	/**
+	 * Process a string of text and return the porcessed data. 
+	 * @param  String - A string of data to be processed
+	 * @return String - returns a string of processed data.
+	 */
 	public String process(String text)
 	{
-		GrammaticalStructure gs = null;
+	    GrammaticalStructure gs = null;
 	    DocumentPreprocessor tokenizer = new DocumentPreprocessor(new StringReader(text));
 	    
-	    for (List<HasWord> sentence : tokenizer) {
-	      List<TaggedWord> tagged = tagger.tagSentence(sentence);
-	      		gs = parser.predict(tagged);
-
-	      		
-	    	}
+	    for (List<HasWord> sentence : tokenizer)
+	    {
+		//Processes word using maxent tagger
+	        List<TaggedWord> tagged = tagger.tagSentence(sentence);
+		//Adds gramatical structure
+	      	gs = parser.predict(tagged);	      		
+	    }
+	    //Gets dependencies
 	    String output = gs.typedDependencies().toString();
+	    //Returns dependancies
 	    return output.equals("") ? "Failed to process this line" : String.format("[Line %s [%s]]", ++lineNumb, output);
 	}
 
