2789f17d788617cd996dec06e7d2e05f9ece0471
WilliamStott <wdstott@hotmail.co.uk>
2017-04-20 13:20:26 +0100

    Updated comments

--- src/OSValidator.java
+++ src/OSValidator.java
@@ -6,51 +6,51 @@ public class OSValidator {
 
     private static String OS = System.getProperty("os.name").toLowerCase();
 
-    	/**
-	 * Checks if is a windows OS. 
-	 *
-	 * @return Bolean - returns true if OS is windows
-	 */
+    /**
+    * Checks if is a windows OS. 
+    *
+    * @return Bolean - returns true if OS is windows
+    */
     public static boolean isWindows() 
     {
         return (OS.indexOf("win") >= 0);
     }
 
-     /**
-	 * Checks if is a Mac OS. 
-	 *
-	 * @return Bolean - returns true if OS is Mac
-	 */
+    /**
+    * Checks if is a Mac OS. 
+    *
+    * @return Bolean - returns true if OS is Mac
+    */
     public static boolean isMac() 
     {
         return (OS.indexOf("mac") >= 0);
     }
 
      /**
-	 * Checks if is a Unix OS. 
-	 *
-	 * @return Bolean - returns true if OS is Unix
-	 */
+    * Checks if is a Unix OS. 
+    *
+    * @return Bolean - returns true if OS is Unix
+    */
     public static boolean isUnix() 
     {
         return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
     }
 
-      /**
-	 * Checks if is a Solaris OS. 
-	 *
-	 * @return Bolean - returns true if OS is Solaris
-	 */
+    /**
+    * Checks if is a Solaris OS. 
+    *
+    * @return Bolean - returns true if OS is Solaris
+    */
     public static boolean isSolaris() 
     {
         return (OS.indexOf("sunos") >= 0);
     }
     
-     /**
-	 * Checks the current OS
-	 *
-	 * @return String - returns a string of the running OS
-	 */
+    /**
+    * Checks the current OS
+    *
+    * @return String - returns a string of the running OS
+    */
     public static String getOS()
     {
         if (isWindows()) {
