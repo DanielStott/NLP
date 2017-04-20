e3500e8e05732c7bc1993bfd78d80e3a3698727f
WilliamStott <wdstott@hotmail.co.uk>
2017-04-20 13:43:15 +0100

    Added comments

--- src/CorePipeline/NER.java
+++ src/CorePipeline/NER.java
@@ -11,11 +11,13 @@ import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 
+//This is the Named Entity Recognition class
 public class NER {
 	private Properties props = new Properties();
 	private StanfordCoreNLP pipeline;
 	private int lineNumb = 0;
 	
+	//Adds properties when constructed
 	public NER()
 	{
 		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
@@ -23,6 +25,11 @@ public class NER {
 		
 	}
 	
+	/**
+	 * Process a string of text and return the porcessed data. 
+	 * @param  String - A string of data to be processed
+	 * @return String - returns a string of processed data.
+	 */
 	public String process(String text)
 	{
 	    // create an empty Annotation just with the given text
@@ -41,12 +48,14 @@ public class NER {
 	    	String ner = "";
 	    	
 	      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+		  //Checks to see if the word is seen as a Name
 	    	  if(!token.get(NamedEntityTagAnnotation.class).equals("O"))
 	    	  {
+			 //Adds Name to string with the original to compare.
 	    		 ner += String.format("[\"%s\" defined as \"%s\"]", token.originalText(), token.get(NamedEntityTagAnnotation.class));
 	    	  }
 	      }
-
+	      //Returns data
 	      return String.format("[Line %s %s]", ++lineNumb, ner);
 	    }
 	   
