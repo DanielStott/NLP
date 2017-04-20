5dabf6ec4066ccab65e263676bd760f636c946ac
WilliamStott <wdstott@hotmail.co.uk>
2017-04-20 13:05:02 +0100

    Update Initialise.java
    Added comments

--- src/Initialise.java
+++ src/Initialise.java
@@ -1,6 +1,14 @@
 
 public class Initialise {
 
+        /**
+     * This function initialises the program on launch
+     *
+     * @String[]  Launch paramaters
+     * @param  name the location of the image, relative to the url argument
+     * @return      the image at the specified URL
+     * @see         Image
+     */
     public static void main(String[] args) throws Exception 
     { 
     	GUI browser = new GUI();
