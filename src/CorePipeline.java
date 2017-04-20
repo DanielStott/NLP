a3e92c48f60b0ed83cbad395de8e759690b0ea7b
Jack-Marsh <Jack.Marsh@live.co.uk>
2017-04-20 14:06:45 +0100

    Update CorePipeline.java

--- src/CorePipeline.java
+++ src/CorePipeline.java
@@ -18,17 +18,19 @@ import java.util.concurrent.Executors;
 
 public class CorePipeline 
 {
-
+	//inner class to store a single instance of an annotator
 	public class ClassStore<classObject, classInstance> {
 	    public Class<?> classObject;
 	    public Object classInstance;
-	    
+		
+	    //constructor
 	    public ClassStore(Class<?> Object, Object Instance) {
 	        this.classObject = Object;
 	        this.classInstance = Instance;
 	      }
 	}
-
+	
+	//list of of the class store instances
 	private ArrayList<ClassStore<Class<?>, Object>> classList = new ArrayList<ClassStore<Class<?>, Object>>();
 
 	public CorePipeline(List<String> annotators) throws Exception
@@ -54,16 +56,20 @@ public class CorePipeline
 		}
 
 	}
-
+	//process file directory
 	public boolean processDataDir(Path dirLocation, Path outputLocation, int outputMode)
 	{
 		try
 		{
+			//array of the different file locations
 			ArrayList<Path> textFileLocations = new ArrayList<Path>();
+			
+			//find all usable file types in directory
 			textFileLocations = ManageFile.mapDirectory(dirLocation, new ArrayList<Path>(), ".txt");
 
 			for(Path filePath : textFileLocations)
 			{
+				//passes each file in the directory to processed
 				processData(filePath, outputLocation, outputMode);
 
 			}
@@ -74,23 +80,25 @@ public class CorePipeline
 			return false;
 		}
 	}
-
+	//process a file
 	public boolean processData(Path fileLocation, Path outputLocation, int outputMode)
 	{
 		try 
-		{
+		{	//read in text from file
 			List<String> rawData = Files.readAllLines(fileLocation, StandardCharsets.UTF_8);
 
 			//Creates a folder of the same name as file being processed		
 			outputLocation = Paths.get(outputLocation.toString(), ManageFile.pathSlash, fileLocation.toString().substring(fileLocation.toString().lastIndexOf(ManageFile.pathSlash) + 1, fileLocation.toString().lastIndexOf('.')));
-
+			
 			if(!Files.exists(outputLocation))
 			{
 				Files.createDirectory(outputLocation);
 			}
-
+			//passes to a map to be processed
 			Map<String , List<String>> allData = processData(rawData); 
+			//save file
 			ManageFile.saveMapToFile(allData, outputLocation);
+			//pass results to GUI
 			GUI.setResult(allData);
 
 
@@ -103,12 +111,16 @@ public class CorePipeline
 		}
 	}
 
+	//process input text or url
 	public boolean processData(List<String> rawData, Path outputLocation)
 	{ 
 		try
 		{
+			//passed to map to processed by annotaotrs
 			Map<String , List<String>> allData = processData(rawData);
+			//save file
 			ManageFile.saveMapToFile(allData, outputLocation); 
+			//display in GUI
 			GUI.setResult(allData);
 		}
 		catch(Exception e)
@@ -119,7 +131,9 @@ public class CorePipeline
 
 		return true;
 	}
-
+	
+	
+	//pass to annotaors
 	public Map<String, List<String>> processData(List<String> rawData)
 	{ 
 		Map<String, List<String>> processedData = new HashMap<String, List<String>>();
